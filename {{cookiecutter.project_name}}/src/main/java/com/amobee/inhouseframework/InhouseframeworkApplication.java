0e675b69cf85ac21b41296f0d47537fb7bc211b0
Suraj Bhadrike <suraj.bhadrike@amobee.com>
2022-12-09 17:30:53 +0530

    first commit

---
+++ {{cookiecutter.project_name}}/src/main/java/com/amobee/inhouseframework/InhouseframeworkApplication.java
@@ -0,0 +1,13 @@
+package com.amobee.inhouseframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InhouseframeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InhouseframeworkApplication.class, args);
+	}
+
+}
---
+++ {{cookiecutter.project_name}}/src/test/java/com/amobee/inhouseframework/InhouseframeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amobee.inhouseframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InhouseframeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
